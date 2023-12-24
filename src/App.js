2c64930b29a5077e7dc4f0f489d1b235bec3eae3
zakaria <zak69164@gmail.com>
2023-12-24 19:24:55 +0100

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,80 +1,87 @@
 import React, { useState, useEffect } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
-import './App.css'; // Assurez-vous d'avoir un fichier App.css avec le style nécessaire
+import './App.css';
 
 function App() {
-  const { control, watch } = useForm({
-    defaultValues: {
-      isPlanet: false,
-      gravity: 1
-    }
-  });
-
+  const { control, watch, setValue } = useForm();
   const [bodies, setBodies] = useState([]);
   const [selectedBody, setSelectedBody] = useState(null);
-
-  const isPlanetChecked = watch('isPlanet');
-  const gravityValue = watch('gravity');
+  const isPlanet = watch('isPlanet');
+  const gravity = watch('gravity');
 
   useEffect(() => {
     axios.get('https://api.le-systeme-solaire.net/rest/bodies')
       .then(response => {
-        const filteredBodies = response.data.bodies.filter(body => {
-          return (isPlanetChecked ? body.isPlanet : true) && body.gravity <= gravityValue;
-        });
+        const filteredBodies = response.data.bodies.filter(body => 
+          (isPlanet ? body.isPlanet : true) && (!gravity || body.gravity <= gravity)
+        );
         setBodies(filteredBodies);
       })
       .catch(error => {
         console.error("Error fetching data: ", error);
       });
-  }, [isPlanetChecked, gravityValue]);
+  }, [isPlanet, gravity]);
 
-  const handleBodySelection = (event) => {
-    const bodyID = event.target.value;
-    const bodyInfo = bodies.find(body => body.id === bodyID);
-    setSelectedBody(bodyInfo);
+  useEffect(() => {
+    setValue('body', '');
+    setSelectedBody(null);
+  }, [bodies, setValue]);
+
+  const handleBodyChange = e => {
+    const body = bodies.find(b => b.id === e.target.value);
+    setSelectedBody(body);
   };
 
   return (
     <div className="App">
       <h1>RHOBS Challenge</h1>
       <form>
-        <label>
-          Is Planet
-          <Controller
-            name="isPlanet"
-            control={control}
-            render={({ field }) => <input type="checkbox" {...field} />}
-          />
-        </label>
-        <label>
-          Gravity
-          <Controller
-            name="gravity"
-            control={control}
-            render={({ field }) => <input type="range" {...field} min="0" max="5" step="0.1" />}
-          />
-        </label>
+        <div className="form-row">
+          <label className="checkbox-label">
+            Is Planet
+            <Controller
+              name="isPlanet"
+              control={control}
+              render={({ field }) => <input type="checkbox" {...field} />}
+            />
+          </label>
+          <label className="slider-label">
+            <Controller
+              name="gravity"
+              control={control}
+              render={({ field }) => <input type="range" {...field} />}
+            />
+            gravity
+          </label>
+        </div>
         <label>
           Bodies:
-          <select onChange={handleBodySelection}>
-            {bodies.map(body => (
-              <option key={body.id} value={body.id}>{body.name}</option>
-            ))}
-          </select>
+          <Controller
+            name="body"
+            control={control}
+            render={({ field }) => (
+              <select {...field} onChange={handleBodyChange}>
+                <option value="">Select a body</option>
+                {bodies.map((body) => (
+                  <option key={body.id} value={body.id}>{body.name}</option>
+                ))}
+              </select>
+            )}
+          />
         </label>
+        {selectedBody && (
+          <div className="info-box">
+            <h2>Info on the body</h2>
+            <p>Name: {selectedBody.name}</p>
+            <p>Gravity: {selectedBody.gravity ? `${selectedBody.gravity} m/s²` : 'N/A'}</p>
+            {/* Autres informations */}
+          </div>
+        )}
       </form>
-      {selectedBody && (
-        <div className="body-info">
-          <h2>Info on the body</h2>
-          <p>Name: {selectedBody.name}</p>
-          <p>Gravity: {selectedBody.gravity} m/s²</p>
-          {/* Affichez ici d'autres informations du corps sélectionné */}
-        </div>
-      )}
     </div>
   );
 }
 
 export default App;
+
